1dd69f8e70911047c7a5f75144f6459c7a05c86a
Tzu-Lun Wong <twon069@gmail.com>
2013-08-07 21:25:01 +1000

    Added nodejs express install

--- setup.sh
+++ setup.sh
@@ -17,6 +17,10 @@ nvm use v0.10.15
 # http://jshint.com/
 npm install -g jshint
 
+# Install express for nodejs
+npm install express
+
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
@@ -32,17 +36,17 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
-cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
-fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+#cd $HOME
+#if [ -d ./dotfiles/ ]; then
+#    mv dotfiles dotfiles.old
+#fi
+#if [ -d .emacs.d/ ]; then
+#    mv .emacs.d .emacs.d~
+#fi
+#git clone https://github.com/startup-class/dotfiles.git
+#ln -sb dotfiles/.screenrc .
+#ln -sb dotfiles/.bash_profile .
+#ln -sb dotfiles/.bashrc .
+#ln -sb dotfiles/.bashrc_custom .
+#ln -sf dotfiles/.emacs.d .
 
